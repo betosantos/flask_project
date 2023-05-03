be3c1b60976194fbca47a2415b668bebcea29dfb
betosantos <oliveira.roberto@gmail.com>
2023-05-03 11:41:53 -0300

    usando vscode

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h2>Hello, Olá Beto oi José !!</h2>"
+    return "<h2>Hello, Olá José Commit usando Vscode !!</h2>"
+
 
 
 if __name__ == '__main__':
